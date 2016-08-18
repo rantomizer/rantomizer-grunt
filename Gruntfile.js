0d18f8c3f46ba0a513aa7f0d39527a35bb184454
Johnny <john.malizia@gmail.com>
2016-08-17 22:44:48 -0700

    Added configuration for some basic url scanning Added support for using Gruntfile configuration Added support for scanning multiple urls at once

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,68 +6,40 @@
  * Licensed under the MIT license.
  */
 
-'use strict';
+'use strict'
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>'
-      ],
-      options: {
-        jshintrc: '.jshintrc'
-      }
-    },
-
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp']
-    },
 
     // Configuration to be run (and then tested).
     rantomizer: {
-      default_options: {
-        options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
+      production: {
+	      urls: ['http://localhost/wsj?env=prod', 'http://localhost/wsj?env=prod2']
       },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!'
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
+      staging: {
+        urls: ['http://localhost/wsj?env=stage']
+      },
+      development: {
+        urls: ['http://localhost/wsj?env=dev']
       }
-    },
-
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
     }
 
-  });
+  })
 
   // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+  grunt.loadTasks('tasks')
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint')
+  grunt.loadNpmTasks('grunt-contrib-clean')
+  grunt.loadNpmTasks('grunt-contrib-nodeunit')
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'rantomizer', 'nodeunit']);
+  grunt.registerTask('test', ['clean', 'rantomizer', 'nodeunit'])
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
-
-};
+  grunt.registerTask('default', ['jshint', 'test'])
+}
--- tasks/rantomizer.js
+++ tasks/rantomizer.js
@@ -1,31 +1,53 @@
 'use strict'
-var request = require('request')
+const url = require('url')
 const TunnelClient = require('../../sel/tunnelclient')
 
 module.exports = function (grunt) {
 
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
-  grunt.registerTask('rantomizer', 'description', function () {
+  grunt.registerMultiTask('rantomizer', 'description', function () {
     // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
+    let done = this.async()
+    let clients = {}
+    let requestCount = 0
+    let expected = this.data.urls.length
+    grunt.log.writeln(`Scanning ${this.data.urls}`)
+    this.data.urls.forEach(function (urlString) {
+      let parsedUrl = url.parse(urlString)
+      let key = parsedUrl.hostname + parsedUrl.port
+      if (!(key in clients)) {
+        clients[key] = []
+      }
+      clients[key].push(parsedUrl)
     })
-    var done = this.async()
-    const client = new TunnelClient('ws://scan.rantomizer.com', '80', () => {
-      client.scan('wsj', (blocks) => {
-        if (blocks.length == 0) {
-          grunt.log.writeln('wsj PASSED')
-        } else {
-          grunt.log.error(`wsj FAILED (${blocks.length})`)
-          blocks.forEach(function (block) {
-            grunt.log.error(`Type: ${block.type} Filter: ${block.filter} Domain: ${block.docDomain} Request: ${block.url}`)
+    // var done = this.async()
+    for (var k in clients) {
+      if (!clients.hasOwnProperty(k)) {
+        // The current property is not a direct property of p
+        continue
+      }
+      let urlArray = clients[k]
+      let client = new TunnelClient('ws://scan.rantomizer.com', urlArray[0].hostname, urlArray[0].port == null ? 80 : urlArray[0].port, () => {
+        urlArray.forEach(function (urlObject) {
+          client.scan(urlObject.path, (blocks) => {
+		requestCount++
+            if (blocks.length == 0) {
+              grunt.log.writeln(`${urlObject.href} PASSED`)
+            } else {
+              grunt.log.error(`${urlObject.href} FAILED (${blocks.length})`)
+              blocks.forEach(function (block) {
+                grunt.log.error(`Type: ${block.type} Filter: ${block.filter} Domain: ${block.docDomain} Request: ${block.url}`)
+              })
+              grunt.warn(`${urlObject.href} FAILED (${blocks.length})`)
+            }
+		if(requestCount == expected) {
+			console.log(`Finished ${requestCount}/${expected}`)
+			done();
+		}
           })
-          grunt.warn(`wsj FAILED (${blocks.length})`)
-        }
-        done()
+        })
       })
-    })
+    }
   })
 }
